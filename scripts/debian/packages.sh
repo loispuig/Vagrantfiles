0020c9db331d9e1dd1a8f41dc28d6c5942e86c4e
Loïs <lois.puig@kctus.fr>
2016-01-31 23:02:21 +0100

    Added curl package

--- scripts/debian/packages.sh
+++ scripts/debian/packages.sh
@@ -4,5 +4,8 @@ echo "installing screenfetch"
 apt-get --yes install screenfetch
 echo "screenfetch" >> /home/vagrant/.bashrc
 
+echo "installing curl"
+apt-get --yes install curl
+
 echo "installing puppet"
 apt-get --yes install puppet
