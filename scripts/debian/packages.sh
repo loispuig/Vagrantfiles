ee5fd87d38e25e0281130cb85602467c0bb41f2d
Loïs <lois.puig@kctus.fr>
2016-02-01 11:54:54 +0100

    Removed curl

--- scripts/debian/packages.sh
+++ scripts/debian/packages.sh
@@ -4,9 +4,6 @@ echo "installing screenfetch"
 apt-get --yes install screenfetch
 echo "screenfetch" >> /home/vagrant/.bashrc
 
-echo "installing curl"
-apt-get --yes install curl
-
 echo "installing puppet"
 apt-get --yes install puppet
 
