1e7b32124933d07d36caee1f96322cd255cf814c
Loïs <lois.puig@kctus.fr>
2016-05-13 10:02:04 +0200

    Add bash aliases to root account

--- scripts/debian/aliases.sh
+++ scripts/debian/aliases.sh
@@ -1,3 +1,20 @@
+cat <<EOF >> /root/.bashrc
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+EOF
+
+cat <<EOF > /root/.bash_aliases
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ll='ls -CF'
+alias l='ls -lA'
+EOF
+
 cat <<EOF > /home/vagrant/.bash_aliases
 alias ls='ls --color=auto'
 alias dir='dir --color=auto'
